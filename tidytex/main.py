3473f597846b15fa8d61593bd26a7a18292131e1
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-13 14:11:28 +0200

    put all modifiers in functions

--- tidytex/main.py
+++ tidytex/main.py
@@ -17,38 +17,46 @@ def _remove_trailing_whitespace(string):
     return string
 
 
-def clean(string):
+def _remove_multiple_spaces(string):
+    return re.sub(" +", " ", string)
 
-    out = string
 
-    out = _remove_comments(out)
-    out = _remove_trailing_whitespace(out)
-
-    # Check for {\it ... }
-    out = out.replace("{\\it ", "\\itshape{")
-
-    # Replace multiple spaces by one
-    out = re.sub(" +", " ", out)
-
-    # TODO remove comments
-    # TODO remove multiple newlines
-
-    # Replace $$...$$ by \[...\]
+def _replace_dollar_dollar(string):
+    '''Replace $$...$$ by \[...\].
+    '''
     p = re.compile("\\$\\$")
-    locations = [m.start() for m in p.finditer(out)]
+    locations = [m.start() for m in p.finditer(string)]
     do_open = True
     offset = 0
     for loc in locations:
         insert = "\\[" if do_open else "\\]"
         off = 0
-        if out[loc - 1 + offset] != "\n":
+        if string[loc - 1 + offset] != "\n":
             insert = "\n" + insert
             off += 1
-        if out[loc + 2 + offset] != "\n":
+        if string[loc + 2 + offset] != "\n":
             insert += "\n"
             off += 1
-        out = out[: loc + offset] + insert + out[loc + 2 + offset :]
+        string = string[: loc + offset] + insert + string[loc + 2 + offset :]
         do_open = not do_open
         offset += off
+    return string
+
+
+def _replace_obsolete_text_mods(string):
+    string = string.replace("{\\it ", "\\itshape{")
+    return string
+
+
+def clean(string):
+    out = string
+
+    out = _remove_comments(out)
+    out = _remove_trailing_whitespace(out)
+    out = _remove_multiple_spaces(out)
+    out = _replace_dollar_dollar(out)
+    out = _replace_obsolete_text_mods(out)
+
+    # TODO remove multiple newlines
 
     return out
