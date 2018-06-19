5eb8f6ba77e189810a530190af90dcf34bb174c9
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-19 18:55:37 +0200

    black fix

--- test/test_cli.py
+++ test/test_cli.py
@@ -7,14 +7,14 @@ import blacktex
 
 def test_cli():
     infile = tempfile.NamedTemporaryFile().name
-    with open(infile, 'w') as f:
+    with open(infile, "w") as f:
         f.write("a+b=c")
 
     outfile = tempfile.NamedTemporaryFile().name
 
     blacktex.cli.main([infile, outfile])
 
-    with open(outfile, 'r') as f:
+    with open(outfile, "r") as f:
         line = f.read()
         assert line == "a+b = c"
 
