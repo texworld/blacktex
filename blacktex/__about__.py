b49f52d5892f39b40c9a36697107e9e77828d740
Nico Schlömer <nico.schloemer@gmail.com>
2019-08-07 10:11:17 +0200

    version, status bump

--- blacktex/__about__.py
+++ blacktex/__about__.py
@@ -1,7 +1,7 @@
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 __author__ = "Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
 __copyright__ = "Copyright (c) 2018-2019, {} <{}>".format(__author__, __author_email__)
 __website__ = "https://github.com/nschloe/blacktex"
 __license__ = "License :: OSI Approved :: MIT License"
-__status__ = "Development Status :: 3 - Alpha"
+__status__ = "Development Status :: 4 - Beta"
