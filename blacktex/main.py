078688cb20eeb7297287e355cd44f01f98179579
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-22 12:11:56 +0200

    pyupgrade

--- blacktex/main.py
+++ blacktex/main.py
@@ -266,7 +266,7 @@ def _replace_def_by_newcommand(string):
     for m in p.finditer(string):
         ranges.append((m.start(), m.end()))
         replacements.append(
-            "\\newcommand{{{}}}".format(string[m.start() + 4 : m.end()])
+            f"\\newcommand{{{string[m.start() + 4 : m.end()]}}}"
         )
 
     return _substitute_string_ranges(string, ranges, replacements)
